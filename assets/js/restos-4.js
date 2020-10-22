facce286d798d07a7450e5b49b01baa19e87e07f
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-10-21 20:12:54 -0400

    zomato api calling, 403

--- assets/js/restos-4.js
+++ assets/js/restos-4.js
@@ -1,7 +1,40 @@
+// ZOMATO API KEY //
+const api= {
+    key: "211a37c27d46db44c2e65076d081b87b",
+    base: "https://cors-anywhere.herokuapp.com/"+"https://developers.zomato.com/api/v2.1/",
+    crossOrigin: null
+}
 
-let cuisinesDiv = document.getElementById('cuisines')
-fetch('https://developers.zomato.com/api/v2.1/search')
-.then(response = response.json())
-.then(cuisine = {
-    cuisinesDiv.innerHTML += `<p> ${cuisine.cuisine} </p>`
-})
+//GOOGLE MAPS API KEY //
+const api= {
+    key: "AIzaSyDHWqTqHS4iUxPTAddHwqSOOHJ4XDGO0qc",
+    base:" ",
+    crossOrigin: null
+}
+// THIS IS THE INPUT BOX CLICK FUNCTION //
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+//ACTION ON PRESSING ENTER BUTTON//
+function setQuery(evt) {
+    if (evt.keyCode === 13) {
+        getResults(searchbox.value);
+        //console.log(searchbox.value);
+    }
+}
+
+//FUNCTION TO FETCH ZOMATO API //
+function getResults (query) {
+    fetch(`${api.base}/cuisine?q=${query}&APPID=${api.key}`)
+    .then(cuisine => {
+        return cuisine.json();
+        }).then (displayResults);  
+    }
+//DISPLAY ZOMATO API RESULTS //
+function displayResults (cuisine) {
+    console.log(cuisine);
+}
+
+
+
+
